aa1273198ff67f555124e317208e2ae442411407
SADIK Brahim <sadbrah@gmail.com>
2025-07-21 15:14:17 +0100

    Added shadcn and dark theme support

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ThemeToggler } from "@/components/theme-toggler";
+
+const NavBar = () => {
+  return (
+    <div>NavBar <ThemeToggler /></div>
+  )
+}
+
+export default NavBar;
